3e7399c120695db8b475ada539d2bacaec9fae19
ambersun1234 <ambersun1019.shawn@gmail.com>
2020-09-20 21:24:50 +0800

    Add circular queue implementation and unit test

---
+++ struct_queue/queue.hpp
@@ -0,0 +1,192 @@
+#include <iostream>
+
+using namespace std;
+
+#define struct_queue_header
+
+#ifndef struct_node_header
+    #include "../struct_node/node.hpp"
+#endif
+
+/*
+ * implement circular queue with doubly linked list
+ */
+
+template<typename itemType>
+class queue {
+    private:
+        node<itemType> *queue_head;
+        node<itemType> *queue_tail;
+        int capacity;
+
+    public:
+        queue();
+        ~queue();
+
+        node<itemType>* getHead();
+        node<itemType>* getTail();
+        int getCapacity();
+
+        itemType front();
+        itemType rear();
+        void enqueue(itemType value);
+        void dequeue();
+        void clean();
+        bool isEmpty();
+        void travel();
+        void reverse();
+};
+
+template<typename itemType>
+queue<itemType>::queue() {
+    this->queue_head = nullptr;
+    this->queue_tail = nullptr;
+    this->capacity = 0;
+}
+
+template<typename itemType>
+node<itemType>* queue<itemType>::getHead() {
+    return this->queue_head;
+}
+
+template<typename itemType>
+node<itemType>* queue<itemType>::getTail() {
+    return this->queue_tail;
+}
+
+template<typename itemType>
+int queue<itemType>::getCapacity() {
+    return this->capacity;
+}
+
+template<typename itemType>
+void queue<itemType>::clean() {
+    if (this->isEmpty()) return;
+
+    node<itemType> *current = this->queue_head;
+    current->getPrevious()->setNext(nullptr);
+
+    while (current != nullptr) {
+        node<itemType> *tmp = current;
+        current = current->getNext();
+
+        delete tmp;
+    }
+
+    this->queue_head = nullptr;
+    this->queue_tail = nullptr;
+    this->capacity = 0;
+    return;
+}
+
+template<typename itemType>
+void queue<itemType>::reverse() {
+    if (this->isEmpty()) return;
+
+    node<itemType> *current = this->queue_head;
+    node<itemType> *backup = nullptr;
+    node<itemType> *backupNext = nullptr;
+
+    while (true) {
+        node<itemType> *tmp = nullptr;
+        backup = current;
+        backupNext = current->getNext();
+
+        tmp = current->getNext();
+        current->setNext(current->getPrevious());
+        current->setPrevious(tmp);
+
+        /* if backupNext is origin head
+         * which means backup is origin tail
+         * which will next become reverse head
+         */
+        if (backupNext == this->queue_head) break;
+        current = backupNext;
+    }
+
+    // currently variable current is at old this->queue_tail position
+    this->queue_tail = backupNext;
+    this->queue_head = backup;
+    return;
+}
+
+template<typename itemType>
+void queue<itemType>::travel() {
+    if (this->isEmpty()) return;
+    
+    node<itemType> *current = this->queue_head;
+    cout << current->getData();
+    current = current->getNext();
+
+    while (current != this->queue_head) {
+        cout << ' ' << current->getData();
+        current = current->getNext();
+    }
+    return;
+}
+
+template<typename itemType>
+itemType queue<itemType>::front() {
+    return this->queue_head->getData();
+}
+
+template<typename itemType>
+itemType queue<itemType>::rear() {
+    return this->queue_tail->getData();
+}
+
+template<typename itemType>
+bool queue<itemType>::isEmpty() {
+    return this->queue_head == nullptr && this->queue_tail == nullptr && this->capacity == 0;
+}
+
+template<typename itemType>
+void queue<itemType>::enqueue(itemType value) {
+    // enqueue insert at rear position
+    node<itemType> *newNode = new node<itemType>(value);
+
+    if (this->isEmpty()) {
+        // queue is empty
+        newNode->setNext(newNode);
+        newNode->setPrevious(newNode);
+
+        this->queue_head = newNode;
+        this->queue_tail = newNode;
+        this->capacity += 1;
+    } else {
+        this->queue_tail->setNext(newNode);
+        newNode->setPrevious(this->queue_tail);
+        newNode->setNext(this->queue_head);
+
+        this->queue_tail = newNode;
+        this->queue_head->setPrevious(this->queue_tail);
+        this->capacity += 1;
+    }
+}
+#include <iostream>
+using namespace std;
+template<typename itemType>
+void queue<itemType>::dequeue() {
+    // dequeue at front position
+    if (this->isEmpty()) return;
+    else {
+        node<itemType> *headNext = this->queue_head->getNext();
+        this->queue_tail->setNext(headNext);
+        headNext->setPrevious(this->queue_tail);
+
+        node<itemType> *tmp = this->queue_head;
+        delete tmp;
+
+        this->queue_head = headNext;
+        this->capacity -= 1;
+
+        if (this->queue_head == this->queue_tail && 
+            this->queue_head != nullptr &&
+            this->capacity == 0) {
+            // no element left
+            this->queue_head = nullptr;
+            this->queue_tail = nullptr;
+        }
+        return;
+    }
+}
---
+++ struct_queue/unit.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#ifndef struct_queue_header
+    #include "./queue.hpp"
+#endif
+
+#ifndef struct_node_header
+    #include "../struct_node/node.hpp"
+#endif
+
+#include "gtest/gtest.h"
+
+class struct_test : public testing::Test {
+    protected:
+        queue<string> *squeue = new queue<string>();
+        queue<int>    *iqueue = new queue<int>();
+        queue<double> *dqueue = new queue<double>();
+};
+
+TEST_F(struct_test, constructor) {
+    EXPECT_TRUE(squeue->getHead() == nullptr);
+    EXPECT_TRUE(squeue->getTail() == nullptr);
+    EXPECT_EQ(squeue->getCapacity(), 0);
+
+    EXPECT_TRUE(dqueue->getHead() == nullptr);
+    EXPECT_TRUE(dqueue->getTail() == nullptr);
+    EXPECT_EQ(dqueue->getCapacity(), 0);
+
+    EXPECT_TRUE(iqueue->getHead() == nullptr);
+    EXPECT_TRUE(iqueue->getTail() == nullptr);
+    EXPECT_EQ(iqueue->getCapacity(), 0);
+}
+
+TEST_F(struct_test, enqueue) {
+    squeue->enqueue("hello");
+    EXPECT_EQ("hello", squeue->front());
+    EXPECT_EQ("hello", squeue->rear());
+    EXPECT_TRUE(squeue->getTail() == squeue->getHead());
+    squeue->enqueue("world");
+    EXPECT_EQ("hello", squeue->front());
+    EXPECT_EQ("world", squeue->rear());
+    EXPECT_EQ(squeue->getCapacity(), 2);
+    EXPECT_EQ(squeue->getHead()->getPrevious(), squeue->getTail());
+    EXPECT_EQ(squeue->getTail()->getNext(), squeue->getHead());
+
+    iqueue->enqueue(100);
+    EXPECT_TRUE(iqueue->getHead() == iqueue->getTail());
+    iqueue->clean();
+    EXPECT_TRUE(iqueue->getHead() == nullptr);
+    EXPECT_TRUE(iqueue->getTail() == nullptr);
+
+    dqueue->enqueue(22.2);
+    dqueue->enqueue(33.3);
+    EXPECT_EQ(dqueue->getHead()->getPrevious(), dqueue->getTail());
+}
+
+TEST_F(struct_test, dequeue) {
+    squeue->clean();
+    EXPECT_EQ(squeue->getHead(), nullptr);
+    squeue->enqueue("hello");
+    squeue->dequeue();
+    EXPECT_EQ(squeue->getTail(), nullptr);
+}
+
+TEST_F(struct_test, front) {
+    squeue->clean();
+    squeue->enqueue("world");
+    squeue->enqueue("hello");
+    squeue->enqueue("pink");
+    EXPECT_EQ(squeue->front(), "world");
+    EXPECT_EQ(squeue->getCapacity(), 3);
+}
+
+TEST_F(struct_test, rear) {
+    squeue->clean();
+    squeue->enqueue("world");
+    squeue->enqueue("hello");
+    squeue->enqueue("pink");
+    EXPECT_EQ(squeue->rear(), "pink");
+    EXPECT_EQ(squeue->getCapacity(), 3);
+
+    iqueue->clean();
+    iqueue->enqueue(10);
+    iqueue->clean();
+    iqueue->enqueue(22);
+    EXPECT_EQ(iqueue->rear(), 22);
+    EXPECT_EQ(iqueue->getHead(), iqueue->getTail());
+
+    dqueue->clean();
+    dqueue->enqueue(22.2);
+    dqueue->enqueue(33.3);
+    dqueue->dequeue();
+    EXPECT_EQ(dqueue->getCapacity(), 1);
+    EXPECT_TRUE(dqueue->getHead() == dqueue->getTail());
+}
+
+TEST_F(struct_test, reverse) {
+    squeue->clean();
+    squeue->enqueue("apple");
+    squeue->enqueue("banana");
+    squeue->enqueue("client");
+    squeue->enqueue("drink");
+    squeue->enqueue("epoxy");
+    node<string> *origin_head = squeue->getHead(), *origin_tail = squeue->getTail();
+    squeue->reverse();
+    EXPECT_EQ(origin_head, squeue->getTail());
+    EXPECT_EQ(origin_tail, squeue->getHead());
+    EXPECT_EQ(squeue->getHead()->getData(), "epoxy");
+    EXPECT_EQ(squeue->getHead()->getNext()->getData(), "drink");
+    EXPECT_EQ(squeue->getHead()->getNext()->getNext()->getData(), "client");
+    EXPECT_EQ(squeue->getTail()->getData(), "apple");
+    EXPECT_TRUE(squeue->getHead()->getPrevious() == squeue->getTail());
+
+    dqueue->clean();
+    dqueue->enqueue(22.2);
+    dqueue->enqueue(33.3);
+    dqueue->dequeue();
+    dqueue->enqueue(66.6);
+    EXPECT_EQ(dqueue->getHead()->getPrevious(), dqueue->getTail());
+    dqueue->reverse();
+    EXPECT_EQ(dqueue->getTail()->getData(), 33.3);
+    EXPECT_EQ(dqueue->getCapacity(), 2);
+
+    iqueue->enqueue(10);
+    iqueue->clean();
+    iqueue->enqueue(20);
+    iqueue->enqueue(30);
+    iqueue->enqueue(40);
+    iqueue->reverse();
+    iqueue->dequeue();
+    EXPECT_EQ(iqueue->getHead()->getData(), 30);
+    EXPECT_EQ(iqueue->getTail()->getData(), 20);
+    EXPECT_EQ(iqueue->getCapacity(), 2);
+    EXPECT_TRUE(iqueue->getHead()->getPrevious() == iqueue->getTail());
+}
+
+GTEST_API_ int main( int argc , char **argv ) {
+	testing::InitGoogleTest( &argc , argv );
+	return RUN_ALL_TESTS();
+}

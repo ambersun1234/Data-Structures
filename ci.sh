a4bb155ae40eda1b56955647d5a63132d325b945
ambersun1234 <ambersun1019.shawn@gmail.com>
2020-10-07 20:12:45 +0800

    Update commit hook

--- ci.sh
+++ ci.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+rt=0
 for element in $(find . -type d -name "struct_*"); do
     if [ "$element" = "./struct_node" ]; then
         continue
     else
         tmp=$(echo ${element} | cut -d'/' -f 2)
         tmp=$(echo ${tmp} | cut -d'_' -f 2)
-        make unit ${tmp}
-        make test ${tmp}
+        make unit ${tmp} >> /dev/null
+        echo ${tmp}
+        make test ${tmp} >> /dev/null
+        rt=$((rt + $?))
     fi
 done
+exit ${rt}
